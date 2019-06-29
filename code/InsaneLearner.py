dee6b900e6e2316dee07e5d6868e2809f5121d99
deepika-sivakumar <50595102+deepika-sivakumar@users.noreply.github.com>
2019-06-29 12:27:48 -0400

    Update InsaneLearner.py

--- code/InsaneLearner.py
+++ code/InsaneLearner.py
@@ -1,17 +1,25 @@
 import numpy as np
 import BagLearner as bl
 import LinRegLearner as lrl
+
 class InsaneLearner(object):
+    
+    # Constructor
     def __init__(self, verbose):
         self.bagLearners = []
         for i in range(20):
             self.bagLearners.append(bl.BagLearner(learner = lrl.LinRegLearner, kwargs = {"verbose":False}, bags = 20, boost = False, verbose = False))
+    
     def author(self):
-        return 'dsivakumar6' # Georgia Tech username
+        return 'Deepika'
+    
+    # Add Training data
     def addEvidence(self,dataX,dataY):
         for i in range(20):
             self.bagLearners[i].addEvidence(dataX, dataY)
         return
+    
+    # Make Predictions
     def query(self,testX):
         predictY = np.empty((0, int(testX.shape[0])), float)
         for i in range(20):
